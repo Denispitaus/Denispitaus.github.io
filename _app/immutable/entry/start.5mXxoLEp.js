aa757e3d4da7e0c7b3d1de0eeed26feddbf193d2
Denis <denisbaskakov508@gmail.com>
2026-07-13 14:50:05 +1000

    Updates

---
+++ _app/immutable/entry/start.5mXxoLEp.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"../chunks/DS0oguL9.js";export{e as load_css,t as start};
--- index.js
+++ index.js
@@ -5,23 +5,23 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="text-scale" content="scale" />
     <link rel="icon" href="/name-вашего-репозитория/img/my-logo.gif" />
-		<link href="/name-вашего-репозитория/_app/immutable/entry/start.Bvt0jlo8.js" rel="modulepreload">
-		<link href="/name-вашего-репозитория/_app/immutable/chunks/Dq0oxUbF.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/entry/start.5mXxoLEp.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/chunks/DS0oguL9.js" rel="modulepreload">
 		<link href="/name-вашего-репозитория/_app/immutable/chunks/DNkNrGWh.js" rel="modulepreload">
-		<link href="/name-вашего-репозитория/_app/immutable/entry/app.BUqmP-U9.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/entry/app.pVUn6B2u.js" rel="modulepreload">
 		<link href="/name-вашего-репозитория/_app/immutable/chunks/xihTtKlq.js" rel="modulepreload">
-		<link href="/name-вашего-репозитория/_app/immutable/nodes/0.JdGCTq_D.js" rel="modulepreload">
-		<link href="/name-вашего-репозитория/_app/immutable/chunks/Bd3mEmsW.js" rel="modulepreload">
-		<link href="/name-вашего-репозитория/_app/immutable/chunks/3QbFAMr2.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/nodes/0.C2ZpVs49.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/chunks/4e-ywpaI.js" rel="modulepreload">
+		<link href="/name-вашего-репозитория/_app/immutable/chunks/D_5hHLu3.js" rel="modulepreload">
 		
-		<link href="/name-вашего-репозитория/_app/immutable/assets/Header.DSRBTnsI.css" rel="stylesheet">
+		<link href="/name-вашего-репозитория/_app/immutable/assets/Header.D4LPVRpX.css" rel="stylesheet">
 		<link href="/name-вашего-репозитория/_app/immutable/assets/0.BBEr02OW.css" rel="stylesheet">
 	</head>
 	<body data-sveltekit-preload-data="hover">
 		<div style="display: contents">
 			<script>
 				{
-					__sveltekit_1fxiquq = {
+					__sveltekit_mya9fh = {
 						base: "/name-вашего-репозитория",
 						assets: "/name-вашего-репозитория"
 					};
@@ -29,8 +29,8 @@
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("/name-вашего-репозитория/_app/immutable/entry/start.Bvt0jlo8.js"),
-						import("/name-вашего-репозитория/_app/immutable/entry/app.BUqmP-U9.js")
+						import("/name-вашего-репозитория/_app/immutable/entry/start.5mXxoLEp.js"),
+						import("/name-вашего-репозитория/_app/immutable/entry/app.pVUn6B2u.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element);
 					});
